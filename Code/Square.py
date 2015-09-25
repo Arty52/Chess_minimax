5db8b00be9e5c37405b38cd9836794696f7d947f
System Administrator <root@Arts-MacBook-Pro.local>
2015-09-25 11:06:30 -0700

    Pieces added are presented to the user

--- Code/Square.py
+++ Code/Square.py
@@ -2,147 +2,144 @@ from string import ascii_uppercase
 
 class Square(object):
 
-	MAX_ROW_BOUND = 7
-	MAX_COL_BOUND = 7
-	MIN_ROW_BOUND = 0
-	MIN_COL_BOUND = 0
+    MAX_ROW_BOUND = 7
+    MAX_COL_BOUND = 7
+    MIN_ROW_BOUND = 0
+    MIN_COL_BOUND = 0
 
-	row = 0
-	column = 0
-	occupied = False
-	Piece = None
+    row = 0
+    column = 0
+    occupied = False
+    Piece = None
 
-	def __init__(self, *args, **kwargs):
-		if len(args) == 2:
-			self.row = args[0]
-			self.column = args[1]
-		else:
-			positionParts = Square.parsePosition(args[0])
-			self.row = positionParts['row']
-			self.column = positionParts['col']
+    def __init__(self, *args, **kwargs):
+        if len(args) == 2:
+            self.row = args[0]
+            self.column = args[1]
+        else:
+            positionParts = Square.parsePosition(args[0])
+            self.row = positionParts['row']
+            self.column = positionParts['col']
 
-	def getRow(self):
-		return self.row
+    def getRow(self):
+        return self.row
 
-	def getColumn(self):
-		return self.column
+    def getColumn(self):
+        return self.column
 
-	def isOccupied(self):
-		return self.occupied
+    def isOccupied(self):
+        return self.occupied
 
-	def removePiece(self):
-		self.occupied = False
+    def removePiece(self):
+        self.occupied = False
 
-		self.Piece = None
+        self.Piece = None
 
-	def assignPiece(self, piece):
-		if piece is None:
-			self.occupied = False
-		else:
-			self.occupied = True
+    def assignPiece(self, piece):
+        if piece is None:
+            self.occupied = False
+        else:
+            self.occupied = True
 
-		self.Piece = piece
+        self.Piece = piece
 
-	def getPiece(self):
-		'''
-		@return: Piece
-		'''
-		return self.Piece
+    def getPiece(self):
+        '''
+        @return: Piece
+        '''
+        return self.Piece
 
-	def generateMoves(self):
+    def generateMoves(self):
 
-		moves = []
+        moves = []
 
-		if self.Piece.type == 'Rook':
-			# All possible row moves
-			for i in range(self.MAX_ROW_BOUND):
-				if i is not self.row:
-					moves.append(Square(i, self.column))
+        if self.Piece.type == 'Rook':
+            # All possible row moves
+            for i in range(self.MAX_ROW_BOUND):
+                if i is not self.row:
+                    moves.append(Square(i, self.column))
             
             # All possible column moves
-			for i in range(self.MAX_COL_BOUND):
-				if i is not self.column:
-					moves.append(Square(self.row, i))
+            for i in range(self.MAX_COL_BOUND):
+                if i is not self.column:
+                    moves.append(Square(self.row, i))
 
-		elif self.Piece.type == 'King':
-			##
-			# Add moves below...
-			##
+        elif self.Piece.type == 'King':
 
-			# Make sure we're still in bounds
-			if self.row - 1 >= self.MIN_ROW_BOUND:
+            # Make sure we're still in bounds
+            if self.row - 1 >= self.MIN_ROW_BOUND:
 
-				newRow = self.row - 1
-				
-				for i in range(self.column - 1, self.column + 2):
-					if i >= self.MIN_COL_BOUND and i <= self.MAX_COL_BOUND:
-						moves.append(Square(newRow, i))
+                newRow = self.row - 1
+                
+                for i in range(self.column - 1, self.column + 2):
+                    if i >= self.MIN_COL_BOUND and i <= self.MAX_COL_BOUND:
+                        moves.append(Square(newRow, i))
 
-			##
-			# Add moves in same row...
-			##
-			for i in range(self.column - 1, self.column + 2):
-				if i >= self.MIN_COL_BOUND and i <= self.MAX_COL_BOUND and i is not self.column:
-					moves.append(Square(self.row, i))
+            ##
+            # Add moves in same row...
+            ##
+            for i in range(self.column - 1, self.column + 2):
+                if i >= self.MIN_COL_BOUND and i <= self.MAX_COL_BOUND and i is not self.column:
+                    moves.append(Square(self.row, i))
 
-			##
-			# Add moves above...
-			##
-			if self.row + 1 <= self.MAX_ROW_BOUND:
+            ##
+            # Add moves above...
+            ##
+            if self.row + 1 <= self.MAX_ROW_BOUND:
 
-				newRow = self.row + 1
-				
-				for i in range(self.column - 1, self.column + 2):
-					if i >= self.MIN_COL_BOUND and i <= self.MAX_COL_BOUND:
-						moves.append(Square(newRow, i))
+                newRow = self.row + 1
+                
+                for i in range(self.column - 1, self.column + 2):
+                    if i >= self.MIN_COL_BOUND and i <= self.MAX_COL_BOUND:
+                        moves.append(Square(newRow, i))
 
-		return moves
+        return moves
 
-	@staticmethod
-	def columnIndex(column):
-		try:
-			column = int(column) - 1
-		except:
-			column = ascii_uppercase.index(column)
+    @staticmethod
+    def columnIndex(column):
+        try:
+            column = int(column) - 1
+        except:
+            column = ascii_uppercase.index(column)
 
-		return column
+        return column
 
-	@staticmethod
-	def indexColumn(column):
-		return ascii_uppercase[column]
+    @staticmethod
+    def indexColumn(column):
+        return ascii_uppercase[column]
 
-	@staticmethod
-	def position(row, column):
-		return ascii_uppercase[int(column)] + str(int(row) + 1)
+    @staticmethod
+    def position(row, column):
+        return ascii_uppercase[int(column)] + str(int(row) + 1)
 
-	@staticmethod
-	def parsePosition(positionDescription):
-		if len(positionDescription) is not 2:
-			raise Exception('Invalid position specified')
+    @staticmethod
+    def parsePosition(positionDescription):
+        if len(positionDescription) is not 2:
+            raise Exception('Invalid position specified')
 
-		col = positionDescription[0]
-		row = positionDescription[1]
+        col = positionDescription[0]
+        row = positionDescription[1]
 
-		# Return 0 based location of the piece
-		return {
-			'col': ("A", "B", "C", "D", "E", "F", "G", "H").index(col),
-			'row': int(row) - 1
-		}
+        # Return 0 based location of the piece
+        return {
+            'col': ("A", "B", "C", "D", "E", "F", "G", "H").index(col),
+            'row': int(row) - 1
+        }
 
-	# Compare if two Square objects are equal (same location)
-	def __eq__(self, otherSquare):
-		return self.row == otherSquare.row and self.column == otherSquare.column
+    # Compare if two Square objects are equal (same location)
+    def __eq__(self, otherSquare):
+        return self.row == otherSquare.row and self.column == otherSquare.column
 
-	def __str__(self):
-		if self.Piece is None:
-			piece = ""
-		else:
-			piece = str(self.Piece)
+    def __str__(self):
+        if self.Piece is None:
+            piece = ""
+        else:
+            piece = str(self.Piece)
 
-		properties = {
-			'piece'  : piece,
-			'row'    : self.row + 1,
-			'column' : ascii_uppercase[self.column],
-		}
+        properties = {
+            'piece'  : piece,
+            'row'    : self.row + 1,
+            'column' : ascii_uppercase[self.column],
+        }
 
-		return '%(piece)s(%(column)s%(row)d)' % properties
+        return '%(piece)s(%(column)s%(row)d)' % properties
--- Code/chessGame.py
+++ Code/chessGame.py
@@ -5,7 +5,7 @@ from Board import Board
 
 GAME_STATES = []
 
-def importBoard():    
+def importBoard():
     try:
         with open('testcase1.txt') as fh:
             if fh:
@@ -24,9 +24,15 @@ def playCase():
     for line in GAME_STATES:
         board = Board()
         try:
+            # Subtract 1 to accomidate indexing
+            print(line)
             board.addPiece('white', 'King', int(line[0])-1, int(line[1])-1)
+            print('White king added: ♕  at {}{}'.format(int(line[0]), printCol(int(line[1]-1))))
             board.addPiece('white', 'Rook', int(line[2])-1, int(line[3])-1)
+            print('White rook added: ♖  at {}{}'.format(int(line[2]), printCol(int(line[3]-1))))
             board.addPiece('black', 'King', int(line[4])-1, int(line[5])-1)
+            print('Black king added: ♛  at {}{}'.format(int(line[4]), printCol(int(line[5]-1))))
+
         except ValueError:
             print()
             print('Your file is not formatted correctly, refer to documentation for more information')
@@ -36,10 +42,16 @@ def playCase():
             
         solve_game(board)
 
+# Takes column value and returns column letter
+def printCol(colVal):
+    cols = ("A", "B", "C", "D", "E", "F", "G", "H")
+    return cols[colVal]
 
 def solve_game(board):
 
     print(board)
+    moves = board.legalMoves('black')
+    print(len(moves))
     # board.setup()
     # print(board)
     # pieces = board.saveState()
@@ -52,6 +64,11 @@ def solve_game(board):
 def main():
     print('Welcome to Art and Adam\'s minimax chess AI!')
     print()
+    
+    '''
+    Show what all three pieces look like
+    '''
+    
     importBoard()
     playCase()
 
